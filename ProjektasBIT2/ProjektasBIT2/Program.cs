4c718807abd4f6c2b60097a2ae937d637862330d
ZivileCernauskiene <labadaba@gmail.com>
2021-09-08 19:12:59 +0300

    bandymas papildyti

--- ProjektasBIT2/ProjektasBIT2/Program.cs
+++ ProjektasBIT2/ProjektasBIT2/Program.cs
@@ -7,6 +7,7 @@ namespace ProjektasBIT2
         static void Main(string[] args)
         {
             Console.WriteLine("______________________________________________________________");
+            int a, b, c;
             Console.WriteLine("Hello World!");
         }
     }
