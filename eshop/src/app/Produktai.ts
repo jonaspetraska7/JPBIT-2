f3f33002feead41315dfd9c0466006b7a9a9c54f
DeimanteK <62334745+Queendi2@users.noreply.github.com>
2021-10-14 21:00:24 +0300

    Sulietuvinti produktai 1/3
    Updatinu trecdali.

--- eshop/src/app/Produktai.ts
+++ eshop/src/app/Produktai.ts
@@ -10,9 +10,9 @@ export interface ProduktoModelis{
   
   export const produktai = [{
     "id": 1,
-    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "title": "Fjallraven -nr. 1 kuprinė,  15 nešiojamųjų kompiuterių",
     "price": 109.95,
-    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "description": "Puiki kuprinė kasdieniam naudojimui ir pasivaikščiojimams miške trumpoms,mažoms iškyloms. Telpa 15 colių nešiojamas kompiuteris ",
     "category": "men's clothing",
     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
     "rating": {
@@ -22,9 +22,9 @@ export interface ProduktoModelis{
 },
 {
     "id": 2,
-    "title": "Mens Casual Premium Slim Fit T-Shirts ",
+    "title": "Vyriški laisvalaikio PREMIUM SLIM marškinėliai ",
     "price": 22.3,
-    "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    "description": "Slim-fit stilius, kontrastingos raglaninės rankovės, trijų sagų „Henley“ plokštelė, lengvas ir minkštas audinys, leidžiantis kvėpuoti ir patogiai dėvėti. Tvirti susiūti marškiniai su apvaliu kaklu, skirti ilgaamžiškumui.Puikiai tinka kasdieniam dėvėjimui ir kietiems beisbolo gerbėjams. Į „Henley“ stiliaus apvalią iškirptę įeina trijų sagų segtukas.",
     "category": "men's clothing",
     "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
     "rating": {
@@ -34,9 +34,9 @@ export interface ProduktoModelis{
 },
 {
     "id": 3,
-    "title": "Mens Cotton Jacket",
+    "title": "Vyriška medvilninė striukė",
     "price": 55.99,
-    "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    "description": "Puiki striukė pavasariui/rudeniui/žiemai.Tinkančios įvairioms progoms, pavyzdžiui: darbui, žygiams pėsčiomis, stovyklavimui, laipiojimui kalnuose/uoloms, važinėjimui dviračiu, kelionėms ar kitoms lauko sąlygoms. Geras dovanų pasirinkimas jums ar jūsų šeimos nariui. Širdinga meilė tėčiui, vyrui ar sūnui per padėkos ar Kalėdų dieną.",
     "category": "men's clothing",
     "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
     "rating": {
@@ -46,9 +46,9 @@ export interface ProduktoModelis{
 },
 {
     "id": 4,
-    "title": "Mens Casual Slim Fit",
+    "title": "Vyrų laisvalaikio slim-fit ",
     "price": 15.99,
-    "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    "description": "Spalva ekrane ir realybeje praktiškai gali šiek tiek skirtis. / Atkreipkite dėmesį, kad kūno formos skiriasi kiekvieno asmenio, todėl išsami informacija apie dydį turėtų būti pateikta žemiau esančiame produkto aprašyme.",
     "category": "men's clothing",
     "image": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
     "rating": {
@@ -58,9 +58,9 @@ export interface ProduktoModelis{
 },
 {
     "id": 5,
-    "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    "title": "John Hardy moterų legenda Naga Gold & Silver Dragon Station apyrankė",
     "price": 695,
-    "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    "description": "Iš mūsų legendos kolekcijos Naga įkvėpė mitinis vandens slibinas, saugantis vandenyno perlą. Dėvėkite viduje, kad būtumėte apdovanotas meile arba į išorę - apsaugai.",
     "category": "jewelery",
     "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
     "rating": {
@@ -70,9 +70,9 @@ export interface ProduktoModelis{
 },
 {
     "id": 6,
-    "title": "Solid Gold Petite Micropave ",
+    "title": "Solidus,smulkus auksinis mikropavas ",
     "price": 168,
-    "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+    "description": "Kokybė garantuota. Grąžinkite arba keiskite bet kokį kita užsakymą per 30 dienų. Sukūrė Hafeez Center JAV.",
     "category": "jewelery",
     "image": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
     "rating": {
