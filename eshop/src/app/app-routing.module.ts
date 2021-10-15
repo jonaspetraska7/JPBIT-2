312d080ea6a0ba5540ce4a1cb91975439bb48470
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2021-10-15 18:14:54 +0300

    Login formu darbai
    *Sukurta registracijos forma
    **Sukurta lauku tikrinimo procedura
    *Sukurta login forma
    ** sukurta nuoroda i password recovery
    *Sukurta password recovery forma

--- eshop/src/app/app-routing.module.ts
+++ eshop/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KontaktaiComponent } from './kontaktai/kontaktai.component';
 import { PagrindinisComponent } from './pagrindinis/pagrindinis.component';
+import { LoginComponent } from './komponentai/login/login.component';
+import { PassRecoverComponent } from './komponentai/pass-recover/pass-recover.component';
+import { RegistracijaComponent } from './komponentai/registracija/registracija.component';
 import { ProduktasComponent } from './produktas/produktas.component';
 import { ProduktuKategorijosComponent } from './produktu-kategorijos/produktu-kategorijos.component';
 
@@ -9,7 +12,10 @@ const routes: Routes = [
   {path: '', component:ProduktasComponent},
   {path: 'pagrindinis', component:PagrindinisComponent},
   {path: 'kontaktai', component:KontaktaiComponent},
-  {path: 'ziviles', component:ProduktuKategorijosComponent}
+  {path: 'ziviles', component:ProduktuKategorijosComponent},
+  {path: 'registracija', component:RegistracijaComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'pass-recover', component:PassRecoverComponent}
 ];
 
 @NgModule({
---
+++ eshop/src/app/komponentai/registracija/registracija.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-registracija',
+  templateUrl: './registracija.component.html',
+  styleUrls: ['./registracija.component.css']
+})
+export class RegistracijaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  pateiktiForma(forma: NgForm) {
+    console.log(forma.value)
+    let json = JSON.stringify(forma.value)
+    console.log(json)
+  }
+}
