9cdb71fe83c987a5fefd4cfad581c4b1983004bd
ZivileCernauskiene <labadaba@gmail.com>
2021-10-21 19:33:20 +0300

    sort bandymas
    padarytos sort funkcijos

--- eshop/src/app/pagrindinis/pagrindinis.component.ts
+++ eshop/src/app/pagrindinis/pagrindinis.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { produktai } from '../Produktai';
+
 
 @Component({
   selector: 'app-pagrindinis',
@@ -10,6 +12,52 @@ export class PagrindinisComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  prekes = produktai;
+
+  pagalKaina = this.prekes.sort(function (a, b): any {
+    return b.price < a.price ? 1 : b.price > a.price ? -1 : 0;
+  });
+
+
+  bandymas() {
+    let byPrice = this.prekes.sort(function (a, b): any {
+      return b.price < a.price ? 1 : b.price > a.price ? -1 : 0;
+    });
+    let byPriceDsc = this.prekes.sort(function (a, b): any {
+      return b.price > a.price ? 1 : b.price < a.price ? -1 : 0;
+    });
+    let byName = this.prekes.sort(function (a, b): any {
+      return b.title < a.title ? 1 : b.title > a.title ? -1 : 0;
+    });
+    let byNameDsc = this.prekes.sort(function (a, b): any {
+      return b.title > a.title ? 1 : b.title < a.title ? -1 : 0;
+    });
+    let byRating =this.prekes.sort(function (a, b): any {
+      return b.rating.rate > a.rating.rate ? 1 : b.rating.rate < a.rating.rate ? -1 : 0;
+    });
+    let byRatingDsc =this.prekes.sort(function (a, b): any {
+      return b.rating.rate < a.rating.rate ? 1 : b.rating.rate > a.rating.rate ? -1 : 0;
+    });
+    let byRatingCount =this.prekes.sort(function (a, b): any {
+      return b.rating.rate > a.rating.rate ? 1 : b.rating.rate < a.rating.rate ? -1 : 0;
+    });
+    let byRatingCountDsc =this.prekes.sort(function (a, b): any {
+      return b.rating.rate > a.rating.rate ? 1 : b.rating.rate < a.rating.rate ? -1 : 0;
+    });
+
+
+
+
+  }
+
+
+  sortPrice() {
+    let naujas = this.prekes.sort(function (a, b): any {
+      return b.price < a.price ? 1 : b.price > a.price ? -1 : 0;
+    });
+    return naujas
   }
 
 }
