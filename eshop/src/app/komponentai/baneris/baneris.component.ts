b773592fef30758f5ba83c350d866e90558b6ab8
ZivileCernauskiene <labadaba@gmail.com>
2021-10-24 20:54:35 +0300

    sticky meniu
    padarytas sticky meniu, uzdetas routerlink ant nuotraukos, turputi pataisytas css

--- eshop/src/app/komponentai/baneris/baneris.component.ts
+++ eshop/src/app/komponentai/baneris/baneris.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-baneris',
-  templateUrl: './baneris.component.html',
-  styleUrls: ['./baneris.component.css']
+    selector: 'app-baneris',
+    templateUrl: './baneris.component.html',
+    styleUrls: ['./baneris.component.css']
 })
 export class BanerisComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        window.addEventListener('scroll', this.scroll, true);
+    }
 
+    ngOnDestroy() {
+        window.removeEventListener('scroll', this.scroll, true);
+    }
+
+    scroll = (): void => {
+        let header = document.getElementById('header');
+        let sticky = header?.offsetHeight;
+
+        if (window.pageYOffset > Number(sticky)) {
+            header?.classList.add('sticky');
+        } else {
+            header?.classList.remove('sticky');
+        }
+    };
 }
