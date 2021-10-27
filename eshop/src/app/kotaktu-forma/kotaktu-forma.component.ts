b392813f9ffdfeed67a68f2038bb9753d0a328aa
AndriusMasiulis <88044819+AndriusMasiulis@users.noreply.github.com>
2021-10-27 21:30:47 +0300

    Zaidimas su kontaktu forma
    *Vis dar nenusiuncia duomenu i Firebase

--- eshop/src/app/kotaktu-forma/kotaktu-forma.component.ts
+++ eshop/src/app/kotaktu-forma/kotaktu-forma.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, Validators, NgForm } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-kotaktu-forma',
@@ -8,13 +9,14 @@ import { FormBuilder, Validators, NgForm } from '@angular/forms';
 })
 export class KotaktuFormaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
 
   ngOnInit(): void {}
   pateiktiForma(manoForma: NgForm){
-    console.log(manoForma.value);
-    let json = JSON.stringify(manoForma.value);
-    console.log(json)
+    let naujaZinute = {
+      vardas: manoForma.value,
+    }
+    this.firestore.collection('projektas').doc('naujaZinute').set(naujaZinute);
     manoForma.reset();
   }
 }
