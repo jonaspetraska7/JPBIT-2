05c29722f613ec9196cd77cb11a1dc544d6d41ad
RobertasT <89936816+RobertasT@users.noreply.github.com>
2021-10-27 21:21:45 +0300

    Update app-routing.module.ts
    Sutvarkiau apsauga. Taip kaip supratau pats...

--- eshop/src/app/app-routing.module.ts
+++ eshop/src/app/app-routing.module.ts
@@ -15,6 +15,31 @@ import { LabirintasComponent } from './labirintas/labirintas.component';
 import { LeftCategoryComponent } from './left-category/left-category.component';
 import { CartComponent } from './cart/cart.component';
 import { PalyginimasComponent } from './komponentai/palyginimas/palyginimas.component';
+import{ AngularFireAuthGuard } from '@angular/fire/compat/auth-AngularFireAuthGuard';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  {path: '', component:ProduktuKategorijosComponent},
+  {path: 'pagrindinis', component:PagrindinisComponent, canActivate: [AngularFireAuthGuard] },
+  {path: 'kontaktai', component:KontaktaiComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'ziviles', component:ProduktuKategorijosComponent, canActivate: [AngularFireAuthGuard]},
+  {path:'memory', component:MemoryGameComponent, canActivate: [AngularFireAuthGuard]},
+  {path:'labirintas', component:LabirintasComponent, canActivate: [AngularFireAuthGuard]},
+
+  {path: 'kategorija', component:ProduktuKategorijosComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'kategorija/:idCategory', component: ProduktuKategorijosComponent, canActivate: [AngularFireAuthGuard]},
+
+  {path: 'produktas/:idProduct', component:ProduktoDetailsComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'karusele', component:KaruseleComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'registracija', component:RegistracijaComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'login', component:LoginComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'pass-recover', component:PassRecoverComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'klaida404', component:Klaida404Component, canActivate: [AngularFireAuthGuard]},
+  {path:'left', component:LeftCategoryComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'cart', component:CartComponent, canActivate: [AngularFireAuthGuard]},
+  {path: 'palyginimas', component:PalyginimasComponent, canActivate: [AngularFireAuthGuard]},
+  {path: '**',   redirectTo: '/klaida404' },
+]
 
 const routes: Routes = [
   {path: '', component:ProduktuKategorijosComponent},
