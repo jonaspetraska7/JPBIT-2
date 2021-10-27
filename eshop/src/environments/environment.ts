c00e30455b724d7c20c94c3188c0618d338e1625
AndriusMasiulis <88044819+AndriusMasiulis@users.noreply.github.com>
2021-10-27 21:04:53 +0300

    Firebase
    *Prideti Firebase moduliai
    *Instaliuotas Firebase npm paketas

--- eshop/src/environments/environment.ts
+++ eshop/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyCOlXwmTITRGC4WzB8dVQxYT-spsYsuAKY",
+  authDomain: "jpbit-2.firebaseapp.com",
+  databaseURL: "https://jpbit-2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "jpbit-2",
+  storageBucket: "jpbit-2.appspot.com",
+  messagingSenderId: "583781389612",
+  appId: "1:583781389612:web:ca075bba1f74b0767c4889"
+}
+
 };
 
 /*
