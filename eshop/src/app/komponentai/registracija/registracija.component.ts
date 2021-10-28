8cbfb423e25d9edac561ddde373b1eed94ab9ec7
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2021-10-28 16:58:39 +0300

    update registracija komponentas

--- eshop/src/app/komponentai/registracija/registracija.component.ts
+++ eshop/src/app/komponentai/registracija/registracija.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AuthService } from 'src/app/auth.service';
 
 
